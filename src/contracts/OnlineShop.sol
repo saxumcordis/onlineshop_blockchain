0ce689cb5a54bd3562f80b642487d886666c35f7
Viktor <fedoseev.v@list.ru>
2020-03-19 13:16:41 +0300

    Comment onlineshop.sol

--- src/contracts/OnlineShop.sol
+++ src/contracts/OnlineShop.sol
@@ -1,21 +1,22 @@
 pragma solidity ^0.5.0;
 
 contract OnlineShop {
-    string public name;
+    string public name; //Свойство name
 
-    uint public productCount = 0;
-    mapping(uint => Product) public products;
+    uint public productCount = 0; //Количество текущих продуктов (свойство)
+    mapping(uint => Product) public products; //"Словарь" ID -> PRODUCT 
 
 
-    struct Product {
-    	uint id;
-    	string name;
-    	uint price;
-    	address payable owner;
-    	bool purchased;
+    struct Product { // Структура продукта
+    	uint id; //ID продукта
+    	string name; //Имя продукта
+    	uint price; //Цена продукта
+    	address payable owner; //Адрес владельца продукта
+    	bool purchased; //Состояние: куплен или нет
     }
 
-    event ProductCreated (
+    event ProductCreated ( // Создаём событие, которое будет посылаться в БлокЧейн, 
+        //чтобы можно было прослушать (узнать о) создание продукта
     	uint id,
     	string name,
     	uint price,
@@ -23,7 +24,8 @@ contract OnlineShop {
     	bool purchased
     );
 
-    event ProductPurchased (
+    event ProductPurchased ( //Создаём событие, которое будет посылаться в БлокЧейн, 
+        //чтобы можно было прослушать (узнать о) покупку продукта
     	uint id,
     	string name,
     	uint price,
@@ -32,48 +34,49 @@ contract OnlineShop {
     );
 
 
-    constructor() public {
+    constructor() public { //Конструктор, который задаёт свойство "ИМЯ" нашему контракту
         name = "Online Shop";
     }	
 
-    function createProduct(string memory _name, uint _price) public { 
-    	// Make sure parameters are correct
-    	//Require a valid name && valid price
+    function createProduct(string _name, uint _price) public { //Функция создания продукта. На вход
+    	// Убеждаемся, что введены непустое имя и неотрицательная цена
     	require(bytes(_name).length > 0 && _price > 0);
-    	// Increment productCount 
+    	// Увеличиваем счётчик продуктов на 1
     	productCount++;
-    	//Create product
+    	// Добавляем продукт в "словарь". msg.sender -- адрес того, кто вызвал эту ф-цию
     	products[productCount] = Product(productCount, _name, _price, msg.sender, false);
-    	//Trigger an event
+    	//Вызываем событие, которое отправлит "транзакцию" в блокчейн
     	emit ProductCreated(productCount, _name, _price, msg.sender, false);
 
     }
 
     function purchaseProduct (uint _id) public payable {
-    	//Fetch the product
+    	//Достанем продукт из нашего списка. "memory" используется, так как это переменная структуры,
+        // которые стандартно задаются storage переменными (вечно хранящимися в блокчейне)
+        // memory-переменная будет очищена после выхода из ф-ции
     	Product memory _product = products[_id];
-    	//Fetch the owner
+    	//Достаём адрес владельца продукта
     	address payable _seller = _product.owner;
-    	//Make sure the product is valid
+    	//Проверяем, правильный ли это продукт (Существует ли он исходя из наших правил организации магазина
+        // ID > 0 и ID < Кол-ва продуктов)
+        // require требует, чтобы условие в скобках выполнялось, иначе функция завершит свою работу на этом этапе.
     	require(_product.id > 0 && _product.id < productCount);
-    	//ENOUGH MONEY?
+    	// Достаточно ли было отправлено денег (msg.value -- количество ETH, которые были переданы при вызове ф-ции)
     	require(msg.value >= _product.price);
-    	//IS IT PURCHASED?
+    	//Проверка, что продукт не был приобретён раннее
     	require(!_product.purchased);
-    	//YOUA RE NOT THE SELLER
+    	//Проверка, чтобы владелец товара не купил его сам у себя
     	require(_seller != msg.sender);
-    	//Transfer ownership to the buyer
+    	//Смена владельца продукта
     	_product.owner = msg.sender;
-    	//Mark as purchased
+    	//Обозначаем продукт приобретённым
     	_product.purchased = true;
-    	//Update the product
+    	//Обновляем продукт в нашем списке
     	products[_id] = _product;
-    	//Pay the seller
+    	//Переводим первому владельцу средства
     	address(_seller).transfer(msg.value);
-    	//Trigger an event
+    	//Вызываем событие, которое отправит "транзакцию" в блокчейн
     	emit ProductPurchased(productCount, _product.name, _product.price, msg.sender, true);
-
-
     }
 }
 
